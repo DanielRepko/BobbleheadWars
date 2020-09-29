d75fa9417a733be93970b26590b7ff96a9a9dbed
Daniel Repko <danielrepko07@gmail.com>
2020-09-29 15:37:59 -0400

    Add turning functionality to marine *added code to PlayerController to make the marine turn to face where the mouse is pointing using raycasting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     private CharacterController characterController;
     public Rigidbody head;
     private Vector3 wobbleDirection;
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -41,7 +43,6 @@ public class PlayerController : MonoBehaviour
         {
             //added my own code to make the head actually wobble instead of just being pushed to the right
             Quaternion headRotation = head.GetComponent<Transform>().localRotation;
-            Debug.Log(headRotation.z);
             //set wobble direction to left if the head is all the way to the right
             if (headRotation.z <= -.2)
             {
@@ -53,9 +54,27 @@ public class PlayerController : MonoBehaviour
                 wobbleDirection = transform.right;
             }
             //add force to the head to make it wobble
-            head.AddForce(wobbleDirection * 150, ForceMode.Acceleration);
+            head.AddForce(wobbleDirection * 150, ForceMode.Acceleration);            
+        }
 
-            //head.AddForce(-transform.right * 150, ForceMode.Acceleration);
+        //creating a raycast
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            //making sure that currentLookTarget is filled with the very last thing that the raycast hit
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+            //getting the position of where the mouse clicked, from the marines perspective (the y is from the marine, not the hit)
+            Vector3 targetPositon = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            //getting the rotation to give the marine to make them turn towards the targetPosition
+            Quaternion rotation = Quaternion.LookRotation(targetPositon - transform.position);
+            //rotating the marine to look at the targetPosition
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10);
         }
     }
 }
