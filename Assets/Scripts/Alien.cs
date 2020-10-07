652e954bc86a4252b8938ed5cce0f8609e591fa2
Daniel Repko <danielrepko07@gmail.com>
2020-10-07 17:17:12 -0400

    Fine tune pathfinding *did some fine tuning to the aliens pathfinding *made it so the aliens despawn when they touch the player or bullets

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -9,6 +9,10 @@ public class Alien : MonoBehaviour
     public Transform target;
     private NavMeshAgent agent;
 
+    //the amount of time when the alien should update its path
+    public float navigationUpdate;
+    private float navigationTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +22,16 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(target != null)
+        navigationTime += Time.deltaTime;
+        if(navigationTime > navigationUpdate)
         {
             agent.destination = target.position;
+            navigationTime = 0;
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
