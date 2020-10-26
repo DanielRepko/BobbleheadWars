e036e1bca7457cffb6b4e1422fd4242d7c0e4103
Daniel Repko <danielrepko07@gmail.com>
2020-10-26 16:51:04 -0400

    Add gun upgrade pickup to the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,19 +13,48 @@ public class GameManager : MonoBehaviour
     public float maxSpawnTime;
     public int aliensPerSpawn;
 
+    // holding the prefab info for the pickup
+    public GameObject upgradePrefab;
+    // the Gun script
+    public Gun gun;
+    // the time before another upgrade can spawn
+    public float upgradeMaxTimeSpawn = 7.5f;
+
     private int aliensOnScreen = 0;
     private float generatedSpawnTime = 0;
     private float currentSpawnTime = 0;
 
+    private bool spawnedUpgrade = false;
+    private float actualUpgradeTime = 0;
+    private float currentUpgradeTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        actualUpgradeTime = Random.Range(upgradeMaxTimeSpawn - 3.0f, upgradeMaxTimeSpawn);
+        actualUpgradeTime = Mathf.Abs(actualUpgradeTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentUpgradeTime += Time.deltaTime;
+
+        if (currentUpgradeTime > actualUpgradeTime)
+        {
+            if (!spawnedUpgrade)
+            {
+                int randomNumber = Random.Range(0, spawnPoints.Length - 1);
+                GameObject spawnLocation = spawnPoints[randomNumber];
+
+                GameObject upgrade = Instantiate(upgradePrefab);
+                Upgrade upgradeScript = upgrade.GetComponent<Upgrade>();
+                upgradeScript.gun = gun;
+                upgrade.transform.position = spawnLocation.transform.position;
+                spawnedUpgrade = true;
+                SoundManager.Instance.PlayOneShot(SoundManager.Instance.powerUpAppear);
+            }
+        }
 
         //code to determine if and when to spawn more aliens
         currentSpawnTime += Time.deltaTime;
