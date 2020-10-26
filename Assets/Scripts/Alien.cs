0ab1c325f496c47b94ad08f5310e98c767d52f6e
Daniel Repko <danielrepko07@gmail.com>
2020-10-26 17:54:48 -0400

    Make aliens drop heads after dieing

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -16,6 +16,9 @@ public class Alien : MonoBehaviour
 
     public UnityEvent OnDestroy;
 
+    public Rigidbody head;
+    public bool isAlive = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,27 +28,40 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        navigationTime += Time.deltaTime;
-        if(navigationTime > navigationUpdate)
+        if (isAlive)
         {
-            agent.destination = target.position;
-            navigationTime = 0;
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                agent.destination = target.position;
+                navigationTime = 0;
+            }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Die();
-        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        if (isAlive)
+        {
+            Die();
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        }        
     }
 
     public void Die()
     {
-        // destroying the gameobject
-        Destroy(gameObject);
-        // invoking OnDestroy
+        isAlive = false;
+        head.GetComponent<Animator>().enabled = false;
+        head.isKinematic = false;
+        head.useGravity = true;
+        head.GetComponent<SphereCollider>().enabled = true;
+        head.gameObject.transform.parent = null;
+        head.velocity = new Vector3(0, 26.0f, 3.0f);
+
         OnDestroy.Invoke();
-        // removing listeners 
         OnDestroy.RemoveAllListeners();
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        Destroy(gameObject);
+        head.GetComponent<SelfDestruct>().Initiate();
     }
 }
